fa5efbe4a11a7fed016d4d13236240905a31eabd
martinsben <martins.benoit.38@gmail.com>
2020-06-15 10:24:14 +0200

    Add bottom navigation bar

--- app/src/main/java/com/example/newsandroid/MainActivity.kt
+++ app/src/main/java/com/example/newsandroid/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.newsandroid
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.work.*
 import com.example.newsandroid.ui.topheadlines.TopHeadlinesFragment
 import com.example.newsandroid.work.RefreshDataWorker
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,6 +23,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         delayedInit()
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.topHeadlinesFragment, R.id.everythingFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 /*
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
---
+++ app/src/main/java/com/example/newsandroid/ui/everything/EverythingViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.newsandroid.ui.everything
+
+import androidx.lifecycle.ViewModel
+
+class EverythingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
