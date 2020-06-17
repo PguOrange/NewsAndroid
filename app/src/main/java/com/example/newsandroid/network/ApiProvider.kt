31372c605a3742c4efab9a7e762676ea070450e5
martinsben <martins.benoit.38@gmail.com>
2020-06-17 15:42:01 +0200

    Add ApiURL to gradle

--- app/src/main/java/com/example/newsandroid/network/ApiProvider.kt
+++ app/src/main/java/com/example/newsandroid/network/ApiProvider.kt
@@ -1,11 +1,12 @@
 package com.example.newsandroid.network
 
+import com.example.newsandroid.BuildConfig
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-private val BASE_URL = "http://newsapi.org/v2/"
+private val BASE_URL = BuildConfig.BaseURL
 
 class ApiProvider {
 
