b94d1be7f70482180264863d809a3a15397fc340
martinsben <martins.benoit.38@gmail.com>
2020-07-16 10:21:48 +0200

    Change If in NewsAdapterPagination.kt

--- app/src/main/java/com/example/newsandroid/adapter/NewsAdapterPagination.kt
+++ app/src/main/java/com/example/newsandroid/adapter/NewsAdapterPagination.kt
@@ -83,11 +83,7 @@ class NewsAdapterPagination (private var items : ArrayList<NewsProperty>, newsLi
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (isLoaderVisible) {
-            if (position == items.size - 1) VIEW_TYPE_LOADING else VIEW_TYPE_NORMAL
-        } else {
-            VIEW_TYPE_NORMAL
-        }
+        return if (position == items.size - 1 && isLoaderVisible) VIEW_TYPE_LOADING else VIEW_TYPE_NORMAL
     }
 
     class ViewHolder(itemView: View) : BaseViewHolder(itemView){
